7e4a1867cea2f6e887800fdaa62d02747a3bdbd1
Lautaro Aiello <lautaroaiello2345@gmail.com>
2022-11-13 20:39:02 -0300

    segunda entrega

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-const express = require ("express")
-const app = express()
-const routerProductos = require("./routes/productos")
-const routerCarritos = require("./routes/carritos")
-const mongoose = require ("mongoose")
+const express = require("express");
+const { connect } = require("mongoose");
+const routerProducts = require('./src/routes/routerProducts.js');
+const routerCarts = require('./src/routes/routerCarts.js');
+const mongoose = require("mongoose");
 
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static("./public"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/productos", routerProductos)
-app.use("/api/carritos", routerCarritos)
+app.use('/api/products', routerProducts);
+app.use('/api/cart', routerCarts);
+app.use('*', (req, res) => {
+	const path = req.params;
+	const method = req.method;
+	res.send({ error: -2, descripcion: `ruta '${path[0]}' método '${method}' no implementada` });
+});
 
-
-app.get('*', (req, res) => {
-    res.json("Error: Ruta o metodo no implementado");
-}
-);
-
-app.post('*', (req, res) => {
-    res.json("Error: Ruta o metodo no implementado");
-}
-);
-
-
-try {
-    const url = "mongodb://localhost:27017/ecommerce"
-    const rta = await mongoose.connect(url, {
-        useNewUrlParse: true,
-        useUnifiedTopology: true,
-    })
-    console.log("Base de datos conectada")
+try{
+	// await connect('mongodb://localhost:27017/ecommerce');
+	mongoose.connect("mongodb+srv://ecommerce:backendcoder@cluster0.audzdoj.mongodb.net/?retryWrites=true&w=majority")
+	console.log("Base conectada!!!")
 } catch (error) {
-    console.log(error)
+	console.log(error)
 }
 
-const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, async () => {
+	console.log(`Server running on PORT ${PORT}`);
+});
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
+server.on('error', err => console.log(err));
